9b6ea11934b6d015246f499c43c898bba7b346b2
Phoenyx89 <alessio.pas89@gmail.com>
2014-05-22 13:59:19 +0200

    gestiti i duplicati

--- src/it/uniroma3/giw/Crawler.java
+++ src/it/uniroma3/giw/Crawler.java
@@ -2,6 +2,7 @@ package it.uniroma3.giw;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,23 +16,23 @@ import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Crawler {
-	
+
 	private String firstPageUrl;
 	private DocumentSaver documentSaver;
 	private final int CRAWLER_MAX_LENGTH = 10;
 	private int pageSavedNumbers = 0;
 	private List<HtmlAnchor> pageToVisit;
 	private final double TRESHOLD = 0.2; 
-	
+
 	private Set<String> visitedPage;
-	
+
 	public Crawler(String firstPage, String foldierName){
 		this.firstPageUrl = firstPage;
 		this.documentSaver = new DocumentSaver(foldierName);
 		this.pageToVisit = new LinkedList<HtmlAnchor>();
 		this.visitedPage = new HashSet<String>();
 	}	
-	
+
 	public WebClient getNewWebClient(){
 		WebClient webClient = new WebClient();
 		webClient.getOptions().setCssEnabled(false);
@@ -41,30 +42,33 @@ public class Crawler {
 	}
 
 	public void doCrawling() {
-		
+
 		WebClient webClient = this.getNewWebClient();		
 		HtmlPage firstPage;
 
 		try {
 			firstPage = webClient.getPage(this.firstPageUrl);
 			this.save(firstPage);
+			System.out.println(firstPage.getUrl().toString());
 			this.visitedPage.add(firstPage.getUrl().toString());
 			String s = firstPage.getUrl().toString();
+
+			//this.pageToVisit.addAll(firstPage.getAnchors());
+			this.addToPageToVisit(firstPage.getAnchors(), firstPage);
 			
-			this.pageToVisit.addAll(firstPage.getAnchors());
 			HtmlPage htmlPage = this.getPageFromList(this.pageToVisit, 0);
-			
+
 			while (htmlPage != null){
-				
+
 				try {
-//					System.out.println(htmlPage.getTitleText());
+					//System.out.println(htmlPage.getTitleText());
 					System.out.println(htmlPage.getUrl().toString());
 					this.save(htmlPage);
-						
-					do {
-						htmlPage = this.getNextPage(htmlPage);
-					} while (htmlPage!=null && this.visitedPage.contains(htmlPage.getUrl().toString()));
-					
+
+
+					htmlPage = this.getNextPage(htmlPage);
+
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}				
@@ -76,7 +80,7 @@ public class Crawler {
 
 		webClient.closeAllWindows();
 	}
-	
+
 	/**
 	 * Preleva ed elimina l'oggetto dalla lista
 	 * @param list 
@@ -88,16 +92,17 @@ public class Crawler {
 		list.remove(index);
 		return page;
 	}
-	
+
 
 	private HtmlPage getNextPage(HtmlPage htmlPage) throws IOException{
-		
+
 		//Controllo se ho raggiunto il limite di pagine salvate
 		if (this.pageSavedNumbers == this.CRAWLER_MAX_LENGTH)
 			return null;
-		
+
 		List<HtmlAnchor> newAnchors = htmlPage.getAnchors();
-		
+		this.addToPageToVisit(newAnchors, htmlPage);
+
 		HtmlPage nextPage;
 		int randomIndex;
 		double randomSurfer = Math.random();
@@ -106,19 +111,29 @@ public class Crawler {
 			nextPage = this.getPageFromList(this.pageToVisit, randomIndex);		
 		} else {
 			randomIndex = (int) (Math.random() * newAnchors.size());
-			
+
 			nextPage = this.getPageFromList(newAnchors, randomIndex);
 		}
-		
-		this.pageToVisit.addAll(newAnchors);
+
+		//this.pageToVisit.addAll(newAnchors);
 		return nextPage;		
 	}
 
 	private void save(HtmlPage page) {
 		if (this.documentSaver.save(page)){
 			this.pageSavedNumbers++;		
-			this.visitedPage.add(page.getUrl().toString());
 		}
 	}
 	
+	
+	private void addToPageToVisit(List<HtmlAnchor> newAnchors, HtmlPage htmlPage) throws MalformedURLException {
+		for(HtmlAnchor anchor : newAnchors) {
+			if(!this.visitedPage.contains(HtmlAnchor.getTargetUrl(anchor.getHrefAttribute(), htmlPage).toString())) {
+				System.out.println("aggiungo: "+HtmlAnchor.getTargetUrl(anchor.getHrefAttribute(), htmlPage)+" a "+anchor.getHrefAttribute());
+				this.pageToVisit.add(anchor);
+				this.visitedPage.add(HtmlAnchor.getTargetUrl(anchor.getHrefAttribute(), htmlPage).toString());
+			}
+		}
+	}
+
 }
