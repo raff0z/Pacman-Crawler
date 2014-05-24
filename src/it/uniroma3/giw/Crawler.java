d2cf78f5b2c5304ee4137eafcc472697b20c129c
raff0z <razorzeta@gmail.com>
2014-05-24 12:24:58 +0200

    Fix della next page

--- src/it/uniroma3/giw/Crawler.java
+++ src/it/uniroma3/giw/Crawler.java
@@ -6,11 +6,9 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Set;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.ThreadedRefreshHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -24,13 +22,13 @@ public class Crawler {
 	private List<HtmlAnchor> pageToVisit;
 	private final double TRESHOLD = 0.2; 
 
-	private Set<String> visitedPage;
+	private Set<String> pageToVisitSet;
 
-	public Crawler(String firstPage, String foldierName){
+	public Crawler(String firstPage, String folderName){
 		this.firstPageUrl = firstPage;
-		this.documentSaver = new DocumentSaver(foldierName);
+		this.documentSaver = new DocumentSaver(folderName);
 		this.pageToVisit = new LinkedList<HtmlAnchor>();
-		this.visitedPage = new HashSet<String>();
+		this.pageToVisitSet = new HashSet<String>();
 	}	
 
 	public WebClient getNewWebClient(){
@@ -40,7 +38,9 @@ public class Crawler {
 		webClient.getOptions().setJavaScriptEnabled(false);
 		return webClient;
 	}
-
+	/**
+	 * Inizia il crawling
+	 */
 	public void doCrawling() {
 
 		WebClient webClient = this.getNewWebClient();		
@@ -50,10 +50,8 @@ public class Crawler {
 			firstPage = webClient.getPage(this.firstPageUrl);
 			this.save(firstPage);
 			System.out.println(firstPage.getUrl().toString());
-			this.visitedPage.add(firstPage.getUrl().toString());
-			String s = firstPage.getUrl().toString();
+			this.pageToVisitSet.add(firstPage.getUrl().toString());
 
-			//this.pageToVisit.addAll(firstPage.getAnchors());
 			this.addToPageToVisit(firstPage.getAnchors(), firstPage);
 			
 			HtmlPage htmlPage = this.getPageFromList(this.pageToVisit, 0);
@@ -93,14 +91,19 @@ public class Crawler {
 		return page;
 	}
 
-
+	/**
+	 * Prende la prossima pagina da visitare
+	 * @param htmlPage = la pagina da cui partire
+	 * @return
+	 * @throws IOException
+	 */
 	private HtmlPage getNextPage(HtmlPage htmlPage) throws IOException{
 
 		//Controllo se ho raggiunto il limite di pagine salvate
 		if (this.pageSavedNumbers == this.CRAWLER_MAX_LENGTH)
 			return null;
 
-		List<HtmlAnchor> newAnchors = htmlPage.getAnchors();
+		List<HtmlAnchor> newAnchors = filterAnchors(htmlPage.getAnchors(), htmlPage);
 		this.addToPageToVisit(newAnchors, htmlPage);
 
 		HtmlPage nextPage;
@@ -108,14 +111,13 @@ public class Crawler {
 		double randomSurfer = Math.random();
 		if (randomSurfer<TRESHOLD){
 			randomIndex = (int) (Math.random() * this.pageToVisit.size());
-			nextPage = this.getPageFromList(this.pageToVisit, randomIndex);		
+			nextPage = this.getPageFromList(this.pageToVisit, randomIndex);	
 		} else {
 			randomIndex = (int) (Math.random() * newAnchors.size());
-
+			
 			nextPage = this.getPageFromList(newAnchors, randomIndex);
 		}
 
-		//this.pageToVisit.addAll(newAnchors);
 		return nextPage;		
 	}
 
@@ -125,15 +127,40 @@ public class Crawler {
 		}
 	}
 	
-	
+	/**
+	 * Aggiunge le ancore alla lista delle pagine da visitare, aggiungendogli la parte iniziale dell'Url
+	 * @param newAnchors = la lista di ancore da aggiungere
+	 * @param htmlPage = la pagina iniziale
+	 * @throws MalformedURLException
+	 */
 	private void addToPageToVisit(List<HtmlAnchor> newAnchors, HtmlPage htmlPage) throws MalformedURLException {
 		for(HtmlAnchor anchor : newAnchors) {
-			if(!this.visitedPage.contains(HtmlAnchor.getTargetUrl(anchor.getHrefAttribute(), htmlPage).toString())) {
-				System.out.println("aggiungo: "+HtmlAnchor.getTargetUrl(anchor.getHrefAttribute(), htmlPage)+" a "+anchor.getHrefAttribute());
+		    String target = HtmlAnchor.getTargetUrl(anchor.getHrefAttribute(), htmlPage).toString();
+			if(!this.pageToVisitSet.contains(target)) {
 				this.pageToVisit.add(anchor);
-				this.visitedPage.add(HtmlAnchor.getTargetUrl(anchor.getHrefAttribute(), htmlPage).toString());
+				this.pageToVisitSet.add(target);
 			}
 		}
 	}
+	
+	/**
+	 * Filtra le ancore per rimanere all'interno del dominio della pagina iniziale
+	 * @param anchors = la lista di ancore da filtrare
+	 * @param htmlPage = la pagina da cui parte
+	 * @return
+	 * @throws MalformedURLException
+	 */
+    private List<HtmlAnchor> filterAnchors(List<HtmlAnchor> anchors,
+	    HtmlPage htmlPage) throws MalformedURLException {
+	List<HtmlAnchor> newAnchors = new ArrayList<HtmlAnchor>();
+	for (HtmlAnchor htmlAnchor : anchors) {
+	    String target = HtmlAnchor.getTargetUrl(
+		    htmlAnchor.getHrefAttribute(), htmlPage).toString();
+	    if (target.contains(firstPageUrl)) {
+		newAnchors.add(htmlAnchor);
+	    }
+	}
+	return newAnchors;
+    }
 
 }
