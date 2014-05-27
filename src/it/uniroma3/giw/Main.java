aac71855033fb44f798d84269aac837b6ce95f6a
Phoenyx89 <alessio.pas89@gmail.com>
2014-05-27 23:40:33 +0200

    committo versione mandata da andrea

--- src/it/uniroma3/giw/Main.java
+++ src/it/uniroma3/giw/Main.java
@@ -6,20 +6,40 @@ public class Main {
 		long start = System.currentTimeMillis()/1000;
 		
 		String sportPage = "http://www.tuttomercatoweb.com/serie-a/";
+		String newsPage = "http://www.repubblica.it/cronaca/";
+		String moviePage = "http://www.mymovies.it/film/";
 		String gamePage = "http://www.spaziogames.it/recensioni_videogiochi/";
 		
-		Crawler crawler = new Crawler(sportPage, "sport");
-		crawler.doCrawling();
-		long endSport = System.currentTimeMillis()/1000;		
-		System.out.println("Tempo impiegato per Sport: " + (endSport-start));
+		String sportPathToSkip = "\\?&start=.*";
+		String newsPathToSkip = "[0-9]*/$";
+		String moviePathToSkip = "([^\\d].*|[0-9]{4}/$|.*\\?.*|[0-9]{4}/.+/.+)";		
+		String gamePathToSkip = "(index|articoli).aspx.*"; 
 		
-//		crawler = new Crawler(gamePage, "game");
-//		crawler.doCrawling();
+		Crawler crawler = new Crawler(sportPage, "sport", sportPathToSkip);
+		crawler.doCrawling();
+		
+		crawler = new Crawler(newsPage, "news", newsPathToSkip);
+		crawler.doCrawling();
+		
+		crawler = new Crawler(moviePage, "movie", moviePathToSkip);
+		crawler.doCrawling();
+		
+		crawler = new Crawler(gamePage, "game", gamePathToSkip);
+		crawler.doCrawling();
+		
 		long end = System.currentTimeMillis()/1000;		
-		System.out.println("Tempo impiegato per Games: " + (end-endSport));
 		
 
 		System.out.println("Tempo totale impiegato: " + (end-start));
+
+//		System.out.println("http://www.mymovies.it/film/2014/".matches(moviePage + moviePathToSkip));//true
+//		System.out.println("http://www.mymovies.it/film/drammatici/".matches(moviePage + moviePathToSkip));//true
+//		System.out.println("http://www.mymovies.it/film/2014/lemeraviglie/".matches(moviePage/* + moviePathToSkip*/));//false
+//		
+//		System.out.println("http://www.spaziogames.it/recensioni_videogiochi/index.aspx".matches(gamePage + gamePathToSkip));//true
+//		System.out.println("http://www.spaziogames.it/recensioni_videogiochi/index.aspx?c=multi&p=2".matches(gamePage + gamePathToSkip));//true
+//		System.out.println("http://www.spaziogames.it/recensioni_videogiochi/console_multi_piattaforma/16253/watch-dogs.aspx".matches(gamePage + gamePathToSkip));//false
+
 	}
 
 }
